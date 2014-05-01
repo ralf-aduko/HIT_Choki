0f8b69140f69c4cbfc22aabe8e88084739facec0
Nogamin <nogami@hit.is.kit.ac.jp>
2014-05-01 10:35:24 +0900

    Update StartupScript.js
    サーバーの初期化やサーバーへの接続を行うモジュールを実行した結果、エラーが発生しなかった場合だけ、次のシーンへ飛ぶように変更しました。(エラーチェック機能の実装)

--- StartupScript.js
+++ StartupScript.js
@@ -18,14 +18,18 @@ function OnGUI () {
 	
 	GUI.Label(Rect (10, 50, 200, 20), "port number :");
 	Portnum = GUI.TextField (Rect (10, 70, 200, 20), Portnum, 25);
-	
+
 	if(GUI.Button(Rect (30, 100, 100, 20), "Host")){
-		Network.InitializeServer(8, int.Parse(Portnum),false);
-		Application.LoadLevel("ServerManagement");
+		// add error check
+		if(Network.InitializeServer(8, int.Parse(Portnum),false) == NetworkConnectionError.NoError){
+			Application.LoadLevel("ServerManagement");
+		}
 	}
 	
 	if(GUI.Button(Rect (30, 130, 100, 20), "Client")){
-		Network.Connect(IPaddress, int.Parse(Portnum));
-		Application.LoadLevel("main");
+		// add error check
+		if(Network.Connect(IPaddress, int.Parse(Portnum)) == NetworkConnectionError.NoError){
+			Application.LoadLevel("main");
+		}
 	}
 }
