43fbf9a1f3cb22d02468003e044980b9a4f65ea4
Nogamin <nogami@hit.is.kit.ac.jp>
2014-05-01 14:33:14 +0900

    Update ServerManagementScript.js
    サーバー管理をする画面のスクリプトです。
    ・ボタンの配置を変更しました。

--- ServerManagementScript.js
+++ ServerManagementScript.js
@@ -16,13 +16,15 @@ function OnGUI() {
 	
 	SendText = GUI.TextField (Rect (10, 30, 200, 20), SendText, 25);
 
-	if(GUI.Button(Rect (30, 100, 100, 20), "Send to All")){
+	// Change GUI layout
+	if(GUI.Button(Rect (10, 80, 100, 20), "Send to All")){
 		networkView.RPC("PrintText",RPCMode.All,SendText);
 	}
 
-
-	if(GUILayout.Button ("Disconnect")) {
+	// Change GUI layout
+	if(GUI.Button(Rect(10, 120, 100, 20), "Disconnect")) {
 		Network.Disconnect(5);
 		MasterServer.UnregisterHost();
 	}
+
 }
